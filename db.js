d2b0bfdec6a0b326c4fcd8d2dade3cdaeb8fc923
Eduardo Miquilena <emiquilena@aluxion.com>
2022-12-16 17:48:58 -0400

    clase 32

--- db.js
+++
@@ -1,5 +0,0 @@
-import logger from "./pino.js";
-
-const child = logger.child({ method: "db", hola: "mundo" });
-
-child.error("llamada a la base de datos");
--- pino.js
+++
@@ -1,6 +0,0 @@
-import pino from "pino";
-// * padre
-const logger = pino("logger_pino.log");
-logger.level = "error";
-logger.info("hola del padre");
-export default logger;
